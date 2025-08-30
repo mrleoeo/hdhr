1aeaf3e490589721d9405e4c1fc0bbe9472730eb
mrleoeo <leo_olden@yahoo.com>
2025-08-30 13:36:26 -0400

    Update main.py

--- main.py
+++ main.py
@@ -42,16 +42,16 @@ def delete_recording(title):
 def get_recorded_episodes(title):
     all_recorded_file_info = requests.get(f'http://{url}/recorded_files.json').json()
 
-    unique_recordings = set()
+    #unique_recordings = set()
     
-    for recording in all_recorded_file_info:
-        if recording['Title'] == title:
-            episodes = requests.get(recording['EpisodesURL']).json()
-            for episode in episodes:
-                unique_recordings.add(episode['EpisodeNumber'])
+    #for recording in all_recorded_file_info:
+     #   if recording['Title'] == title:
+      #      episodes = requests.get(recording['EpisodesURL']).json()
+       #     for episode in episodes:
+        #        unique_recordings.add(episode['EpisodeNumber'])
             
-        for episode_name in unique_recordings:
-            print('"' + episode_name + '"')
+        #for episode_name in unique_recordings:
+         #   print('"' + episode_name + '"')
 
 def get_recording_counts(title):
     recordings = requests.get(f'http://{url}/recorded_files.json').json()
