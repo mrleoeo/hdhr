49ae4f03fb9b6d7a0f43184b773c4a53b11130a1
mrleoeo <leo_olden@yahoo.com>
2025-08-31 12:28:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def get_recorded_episodes(title):
         if recording['Title'] == title:
             episodes = requests.get(recording['EpisodesURL']).json()
             for episode in episodes:
-                unique_recordings.add(episode['EpisodeNumber'])
+                unique_recordings.add(episode['CmdURL'])
             
         for episode_name in unique_recordings:
             print('"' + episode_name + '"')
